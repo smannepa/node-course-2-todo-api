4f0f4e17a5decd803a59d0eaf74393683428608d
smannepa <susheel.mannepalli@gmail.com>
2016-12-15 12:36:44 -0800

    Added DELETE /todos/:id

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -4,6 +4,14 @@ const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
 const {mongoose} = require('./../server/db/mongoose');
 
-Todo.remove({}).then((result) => {
-  console.log(result);
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+// Todo.findOneAndRemove().then((todo) => {
+//   
+// });
+
+Todo.findByIdAndRemove('5852f4e20b68d41e1438a8fe').then((todo) => {
+  console.log(todo);
 });
