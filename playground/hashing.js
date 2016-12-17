d1c60c77243376522762a1e9a2f277b2f9ca5859
Susheel Mannepalli <susheel@ChamuMBP.local>
2016-12-17 12:35:07 -0800

    Added password hashing before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,27 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded:', decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   })
+// });
+
+var hashedPassword = '$2a$10$5EkSdjNbwRkbzJ1zcP2kB.WXnRRyaRVvHFpU55UHxECLbDnWE.Du2';
+bcrypt.compare('123abc', hashedPassword, (err, res) => {
+  console.log(res);
+});
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded:', decoded);
 
 // var message = 'I am user number 5';
 // var hash = SHA256(message).toString();
